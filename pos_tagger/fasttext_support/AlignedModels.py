e11df2eb17c1f22939383ee803e071e13f882acd
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-03-13 20:11:22 +1100

    converted AlignedModels.py to use numpy matrix operations to speed things up 1000x+

--- pos_tagger/fasttext_support/AlignedModels.py
+++ pos_tagger/fasttext_support/AlignedModels.py
@@ -7,11 +7,17 @@ TODO: Use https://fasttext.cc/docs/en/aligned-vectors.html
 * Output relative word probabilities
 """
 import numpy as np
+from collections import namedtuple
+
+WordEmbeddings = namedtuple('WordEmbeddings', [
+    'DFreqs', 'DFreqsToWord', 'LVectors'
+])
 
 
 def get_D_word_embeddings(path):
     DWords = {}
     DFreqs = {}
+    DFreqsToWord = {}
 
     with open(path, 'r', encoding='utf-8') as f:
         for x, line in enumerate(f):
@@ -20,95 +26,92 @@ def get_D_word_embeddings(path):
             coefs = np.asarray(values[1:], dtype='float32')
             DWords[word] = coefs
             DFreqs[word] = x  # More common words might be higher up(?)
+            DFreqsToWord[x] = word
+
             if x % 1000 == 0:
                 print(x)
-    return DFreqs, DWords
+
+            #if x > 20000:
+            #    break
+
+    a = np.ndarray(
+        shape=(
+            len(DWords),
+            len(DWords[word])
+        ),
+        dtype='float32'
+    )
+    for word, vec in DWords.items():
+        # Note the frequency is the actual index
+        a[DFreqs[word], :] = vec
+    return WordEmbeddings(DFreqs, DFreqsToWord, a)
 
 
-DEnglishFreqs, DEnglish = get_D_word_embeddings(
+ENGLISH = get_D_word_embeddings(
     '/mnt/docs/dev/data/fast_text/aligned_word_vectors/wiki.en.align.vec'
 )
-DChineseFreqs, DChinese = get_D_word_embeddings(
+CHINESE = get_D_word_embeddings(
     '/mnt/docs/dev/data/fast_text/aligned_word_vectors/wiki.zh.align.vec'
 )
-DIndonesianFreqs, DIndonesian = get_D_word_embeddings(
+INDONESIAN = get_D_word_embeddings(
     '/mnt/docs/dev/data/fast_text/aligned_word_vectors/wiki.id.align.vec'
 )
-DGermanFreqs, DGerman = get_D_word_embeddings(
+GERMAN = get_D_word_embeddings(
     '/mnt/docs/dev/data/fast_text/aligned_word_vectors/wiki.de.align.vec'
 )
 
 
-def get_L_cands(DVectors, find_me):
-    LCands = []
-    for key, value in DVectors.items():
-        LCands.append((abs(np.sum(np.abs(find_me - value))), key))
-    LCands.sort()
-    return LCands[:30]
+def get_L_cands(word_embeddings, find_me):
+    LCands = word_embeddings.LVectors - find_me
+    LCands = np.sum(np.abs(LCands), axis=1)
+    LSmallestIdx = np.argpartition(LCands, 30)[:30]  # ???
+
+    LRtn = []
+    for idx in LSmallestIdx:
+        # (score, frequency/ID)
+        LRtn.append((LCands[idx], word_embeddings.DFreqsToWord[idx]))
+    LRtn.sort()
+    return LRtn
 
 
-def get_diff_cands(DFromVectors, DToVectors,
-                   from_vec, to_vec):
+def print_translations(word_embeddings, s):
+    find_me = ENGLISH.LVectors[ENGLISH.DFreqs[s]]
+    eng_freq = ENGLISH.DFreqs[s]
+    LCands = get_L_cands(word_embeddings, find_me)
 
-    # Note range seems to be -1.0->+1.0
-    not_in_to = (from_vec-to_vec)
-    not_in_from = (to_vec-from_vec)
-
-    min_ = min(to_vec.min(), from_vec.min())
-    max_ = max(to_vec.max(), from_vec.max())
-
-    not_in_to = np.interp(
-        not_in_to,
-        (not_in_to.min(), not_in_to.max()),
-        (min_, max_)  # WARNING!
-    )
-    not_in_from = np.interp(
-        not_in_from,
-        (not_in_from.min(), not_in_from.max()),
-        (min_, max_)
-    )
-
-    print("NOT IN TO:", not_in_to)
-    print("FROM VEC:", from_vec)
-    print("TO VEC:", to_vec)
-
-    return (
-        get_L_cands(DFromVectors, not_in_from),
-        get_L_cands(DFromVectors, not_in_to),
-        get_L_cands(DToVectors, not_in_from),
-        get_L_cands(DToVectors, not_in_to),
-    )
-
-
-def print_translations(DWordVectors, DWordFreqs, s):
-    LCands = get_L_cands(DWordVectors, DEnglish[s])
-
-    L30k = []
-    LGreater = []
+    LLikely = []
+    LUnlikely = []
 
     def do_print(L):
         for score, cand in L:
             print(
                 score, cand,
                 f"English Freq: "
-                f"{DEnglishFreqs[s]}/{len(DEnglishFreqs)}",
+                f"{ENGLISH.DFreqs[s]}/{len(ENGLISH.DFreqs)}",
                 f"Other Freq: "
-                f"{DWordFreqs[cand]}/{len(DWordFreqs)}"
+                f"{word_embeddings.DFreqs[cand]}/{len(word_embeddings.DFreqs)}"
             )
 
     # Really uncommon words are
     # lower quality and likely junk
     for score, cand in LCands:
-        freq = DWordFreqs[cand]
-        if freq < 30000:
-            L30k.append((score, cand))
-        else:
-            LGreater.append((score, cand))
+        freq = word_embeddings.DFreqs[cand]
 
-    print("==In 30000 Most Common Words==")
-    do_print(L30k)
-    print("==Uncommon Words==")
-    do_print(LGreater)
+        if freq > min(eng_freq, 2000)**1.3:
+            # It should be pretty unlikely that the frequency in English
+            # is an order of magnitude higher in the other language
+
+            # (OPEN ISSUE: Perhaps it could be argued, very common words
+            #  in one language may not be the best candidate for
+            #  uncommon words?)
+            LUnlikely.append((score, cand))
+        else:
+            LLikely.append((score, cand))
+
+    print("==Likely candidates==")
+    do_print(LLikely)
+    print("==Unlikely candidates==")
+    do_print(LUnlikely)
 
 
 if __name__ == '__main__':
@@ -119,8 +122,8 @@ if __name__ == '__main__':
     while True:
         find_me = input("Enter a word to find:")
         try:
-            print_translations(DChinese, DChineseFreqs, find_me)
-            print_translations(DIndonesian, DIndonesianFreqs, find_me)
-            print_translations(DGerman, DGermanFreqs, find_me)
+            print_translations(CHINESE, find_me)
+            print_translations(INDONESIAN, find_me)
+            print_translations(GERMAN, find_me)
         except KeyError:
             print(f"{find_me} was not found")
