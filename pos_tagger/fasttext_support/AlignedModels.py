a33d4d02c5e961df396fc2a885dc68e25c86b43f
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2020-03-14 11:54:07 +1100

    added align sentences function to AlignedModels.py

--- pos_tagger/fasttext_support/AlignedModels.py
+++ pos_tagger/fasttext_support/AlignedModels.py
@@ -6,11 +6,18 @@ TODO: Use https://fasttext.cc/docs/en/aligned-vectors.html
 * Perhaps could also allow training my own models?
 * Output relative word probabilities
 """
+from collections import namedtuple
+from sys import maxsize
 import numpy as np
+
+# Putting this here so I don't forget
+# to enable the entire dictionaries
+TEST_MODE = True
 
 
 class AlignedVectors:
     def __init__(self, path):
+        self.iso = path.split('wiki.')[1].split('.')[0]
         self.DFreqs, self.DFreqsToWord, self.LVectors = \
             self.__get_D_word_embeddings(path)
 
@@ -35,7 +42,7 @@ class AlignedVectors:
                 if x % 1000 == 0:
                     print(x)
 
-                if x > 20000:
+                if x > 20000 and TEST_MODE:
                     break
 
         a = np.ndarray(
@@ -142,20 +149,149 @@ class AlignedVectors:
         print("==Unlikely candidates==")
         do_print(LUnlikely)
 
-    #====================================================================#
-    #                        Sentence Alignment                          #
-    #====================================================================#
 
-    def align_sentences(self,
-                        from_iso, to_iso,
-                        from_s, to_s):
-        pass
+#====================================================================#
+#                        Sentence Alignment                          #
+#====================================================================#
+
+
+AlignedItem = namedtuple('AlignedItem', [
+    'source_index', 'target_index',
+    'source_text', 'target_text',
+    'score'
+])
+
+
+def get_tokens(iso, s):
+    # FOR TESTING ONLY!!! =========================================================
+    return s.split()
+
+
+def align_sentences(from_inst, to_inst,
+                    from_s, to_s):
+
+    # Tokenize the two sentences (if they haven't been already)
+    # If using a POS system which tokenizes separately, it might
+    # make sense to just use its output, and not do it again
+    if isinstance(from_s, (list, tuple)):
+        LFromTokens = from_s
+    else:
+        LFromTokens = get_tokens(from_inst.iso, from_s)
+
+    if isinstance(to_s, (list, tuple)):
+        LToTokens = to_s
+    else:
+        LToTokens = get_tokens(to_inst.iso, to_s)
+
+    # Get the vectors for all words we can
+    LFromVecs = []
+    LToVecs = []
+
+    for x, from_token in enumerate(LFromTokens):
+        try:
+            from_vec = from_inst.get_vector_for_word(
+                from_token.lower()
+            )
+        except KeyError:
+            from_vec = None
+        LFromVecs.append(from_vec)
+
+    for y, to_token in enumerate(LToTokens):
+        try:
+            to_vec = to_inst.get_vector_for_word(
+                to_token.lower()
+            )
+        except KeyError:
+            to_vec = None
+        LToVecs.append(to_vec)
+
+    # Get differential scores for all combinations
+    a = np.full(
+        shape=(len(LFromTokens), len(LToTokens)),
+        dtype='float32',
+        fill_value=maxsize
+    )
+    for x, from_token in enumerate(LFromTokens):
+        for y, to_token in enumerate(LToTokens):
+            from_vec = LFromVecs[x]
+            to_vec = LToVecs[y]
+
+            if from_vec is None or to_vec is None:
+                continue  # Already filled with `maxsize`
+            else:
+                diff = np.sum(np.abs(from_vec-to_vec))
+                a[x, y] = diff
+
+    # Get a from/to map of indices in LFromTokens/LToTokens
+    DFromToMap = {}
+    DToFromMap = {}
+
+    while len(DFromToMap) != min(len(LToVecs), len(LFromVecs)):
+        idx1, idx2 = smallest_indices(
+            a, min(len(LFromTokens), len(LToTokens))
+        )
+        #print("IDX:", idx1, idx2)
+
+        for x, y in zip(idx1, idx2):
+            if not x in DFromToMap and not y in DToFromMap:
+                DFromToMap[x] = y
+                DToFromMap[y] = x
+            a[x, y] = maxsize
+
+    # Output aligned tokens (AlignedItem's)
+    # Won't output whitespace for now
+    LFromRtn = []
+    for x, from_token in enumerate(LFromTokens):
+        if x in DFromToMap:
+            LFromRtn.append(AlignedItem(x, DFromToMap[x],
+                                        from_token, LToTokens[DFromToMap[x]],
+                                        a[x, y]))
+        else:
+            LFromRtn.append(AlignedItem(x, None,
+                                        from_token, None,
+                                        None))
+
+    LToRtn = []
+    for y, to_token in enumerate(LToTokens):
+        if y in DToFromMap:
+            LToRtn.append(AlignedItem(y, DToFromMap[y],
+                                      to_token, LFromTokens[DToFromMap[y]],
+                                      a[x, y]))
+        else:
+            LToRtn.append(AlignedItem(y, None,
+                                      to_token, None,
+                                      None))
+    return LFromRtn, LToRtn
+
+
+def smallest_indices(ary, n):
+    """
+    Returns the n smallest indices from a numpy array.
+    """
+    flat = ary.flatten()
+    indices = np.argpartition(flat, n)[:n] # CHECK ME!!
+    indices = indices[np.argsort(flat[indices])]
+    return np.unravel_index(indices, ary.shape)
 
 
 if __name__ == '__main__':
     BASE_PATH = '/mnt/docs/dev/data/fast_text/aligned_word_vectors'
     ENGLISH = AlignedVectors(f'{BASE_PATH}/wiki.en.align.vec')
+
+    print(
+        align_sentences(ENGLISH, ENGLISH,
+                        'I eat cake for breakfast nope',
+                        'Cake I eat breakfast for')
+    )
+
     CHINESE = AlignedVectors(f'{BASE_PATH}/wiki.zh.align.vec')
+
+    print(align_sentences(
+        ENGLISH, CHINESE,
+        'May I eat that cake ?',
+        '我 可以 吃 那个 蛋糕 吗 ？'
+    ))
+
     INDONESIAN = AlignedVectors(f'{BASE_PATH}/wiki.id.align.vec')
     GERMAN = AlignedVectors(f'{BASE_PATH}/wiki.de.align.vec')
 
