1e69363bc2f8502e1e1a0fa22965ed2bcbd29f26
Dave Morrissey <20507948+mcyph@users.noreply.github.com>
2022-09-26 22:48:18 +1000

    convert to new speedysvc version

--- pos_tagger/client_server/POSTaggerClient.py
+++ pos_tagger/client_server/POSTaggerClient.py
@@ -2,9 +2,9 @@ from pos_tagger.toolkit.patterns.Singleton import Singleton
 from speedysvc.client_server.shared_memory.SHMClient import SHMClient
 from speedysvc.client_server.base_classes.ClientMethodsBase import ClientMethodsBase
 
+from pos_tagger.consts import CubeItem, AlignedCubeItem
 from pos_tagger.abstract_base_classes.POSTaggersBase import POSTaggersBase
 from pos_tagger.client_server.POSTaggerServer import CPUPOSTaggerServer as srv
-from pos_tagger.consts import CubeItem, AlignedCubeItem
 
 
 class POSTaggerClient(POSTaggersBase,
@@ -18,9 +18,7 @@ class POSTaggerClient(POSTaggersBase,
         ClientMethodsBase.__init__(self, client_provider)
 
     def get_L_sentences(self, iso, s):
-        LRtn = self.send(
-            srv.get_L_sentences, [iso, s]
-        )
+        LRtn = self.send(srv.get_L_sentences, [iso, s])
         return self.__deserialize_cube_item(LRtn)
 
     def is_iso_supported(self, iso):
@@ -30,16 +28,12 @@ class POSTaggerClient(POSTaggersBase,
         return self.send(srv.get_L_supported_isos, [])
 
     def is_alignment_supported(self, from_iso, to_iso):
-        return self.send(srv.is_alignment_supported, [
-            from_iso, to_iso
-        ])
+        return self.send(srv.is_alignment_supported, [from_iso, to_iso])
 
     def get_aligned_sentences(self,
                               from_iso, to_iso,
                               from_s, to_s):
-        L1, L2 = self.send(srv.get_aligned_sentences, [
-            from_iso, to_iso, from_s, to_s
-        ])
+        L1, L2 = self.send(srv.get_aligned_sentences, [from_iso, to_iso, from_s, to_s])
         L1 = self.__deserialize_cube_item([L1])[0]
         L2 = self.__deserialize_cube_item([L2])[0]
         return L1, L2
@@ -56,14 +50,10 @@ class POSTaggerClient(POSTaggersBase,
         return n_LRtn
 
     def get_similar_words(self, iso, word, n=30):
-        return self.send(srv.get_similar_words, [
-            iso, word, n
-        ])
+        return self.send(srv.get_similar_words, [iso, word, n])
 
     def get_translations(self, from_iso, to_iso, s):
-        return self.send(srv.get_translations, [
-            from_iso, to_iso, s
-        ])
+        return self.send(srv.get_translations, [from_iso, to_iso, s])
 
     def fasttext_get_num_words(self, iso):
         return self.send(srv.fasttext_get_num_words, [iso])
--- pos_tagger/client_server/POSTaggerServer.py
+++ pos_tagger/client_server/POSTaggerServer.py
@@ -1,60 +1,54 @@
 from pos_tagger.POSTaggers import POSTaggers
 
-from speedysvc.rpc_decorators import json_method
-from speedysvc.client_server.base_classes.ServerMethodsBase import ServerMethodsBase
+from speedysvc.service_method import service_method
 
 
-class CPUPOSTaggerServer(ServerMethodsBase):
-    port = 40519
-    name = 'postag'
+class CPUPOSTaggerServer:
+    #port = 40519
+    #name = 'postag'
 
-    def __init__(self, logger_client, use_gpu=False):
+    def __init__(self, use_gpu=False):
         """
         A server which e.g. allows putting the POS Tagger on
         a server which has a GPU for POS tagging acceleration
         """
-        ServerMethodsBase.__init__(self, logger_client)
         self.pos_taggers = POSTaggers(use_gpu=use_gpu)
 
-    @json_method
+    @service_method()
     def get_L_sentences(self, iso, s):
         return self.pos_taggers.get_L_sentences(iso, s)
 
-    @json_method
+    @service_method()
     def is_iso_supported(self, iso):
         return self.pos_taggers.is_iso_supported(iso)
 
-    @json_method
+    @service_method()
     def get_L_supported_isos(self):
         return self.pos_taggers.get_L_supported_isos()
 
-    @json_method
+    @service_method()
     def is_alignment_supported(self, from_iso, to_iso):
-        return self.pos_taggers.is_alignment_supported(
-            from_iso, to_iso
-        )
+        return self.pos_taggers.is_alignment_supported(from_iso, to_iso)
 
-    @json_method
+    @service_method()
     def get_aligned_sentences(self,
                               from_iso, to_iso,
                               from_s, to_s):
-        return self.pos_taggers.get_aligned_sentences(
-            from_iso, to_iso, from_s, to_s
-        )
+        return self.pos_taggers.get_aligned_sentences(from_iso, to_iso, from_s, to_s)
 
-    @json_method
+    @service_method()
     def get_similar_words(self, iso, word, n=30):
         return self.pos_taggers.get_similar_words(iso, word, n)
 
-    @json_method
+    @service_method()
     def get_translations(self, from_iso, to_iso, s):
         return self.pos_taggers.get_translations(from_iso, to_iso, s)
 
-    @json_method
+    @service_method()
     def fasttext_get_num_words(self, iso):
         return self.pos_taggers.fasttext_get_num_words(iso)
 
-    @json_method
+    @service_method()
     def get_fasttext_words(self, iso, exclude_high_freq=True):
         return self.pos_taggers.get_fasttext_words(iso, exclude_high_freq)
 
